c5594304ab3102e893cac06f8688808ffc1869a5
Jorge Lazaro <skaterhater.16772@gmail.com>
2025-01-09 19:08:07 -0600

    created post and get request for quotes

--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,60 @@
 console.log("hello world");
+
+//Axios lives on the client side, 
+//It allows us to make requests to our server from our client
+
+//button click
+// we can make a request to the back end using a function
+
+function getQuotes() {
+    // we want s function that gets quotes from the server using axios
+    // we want to use axios inside this function
+    //we are chaining axios methods here
+    // first we reach out to the endpoint
+    // then we wait for a response, and we do something 
+    // .then handles the succesful case, where we got data
+    //.catch handles the failure case
+    axios
+    .get('/quotes')
+    .then((response) => {
+        console.log(
+            // the actual data lives in the response.data 
+            response.data
+            //response
+        )
+        let quotesFromServer = response.data
+        renderToDOM(quotesFromServer)
+    })
+    .catch((error) => {
+        console.log(error)
+        alert('something went wrong')
+    })
+    console.log('in quotes function')
+}
+getQuotes()
+
+// axios will call the endpoint, wait for the response, thats what .then means
+//Axios lets us call from the front end.
+
+
+//render means i want to show data on the dom,
+// so we will render data on the dom
+
+
+//in index html ill need to create something 
+// the thing will need to have an id
+// some piece of daataa needs too get appended
+
+function renderToDOM(quotes) {
+    let contentDiv = 
+        document.querySelector('#content')
+    contentDiv.innerHTML = ''
+    for (let quote of quotes) {
+        contentDiv.innerHTML += `
+        <p> "${quote.text}" -${quote.author}</p>
+        `
+    }
+
+    console.log('in renderToDOM')
+}
+// we get id with a hashtag when we use queryselector
--- server/server.js
+++ server/server.js
@@ -8,7 +8,45 @@ const port = 5001;
 // express static file serving - public is the folder name
 app.use(express.static("server/public"));
 
+// if we are POSTing data, we need to use this bioler plate
+// javascript object notation
+//serialization, deserialization
+app.use(express.json())
+
 // Start up our server
 app.listen(port, () => {
   console.log("listening on port", port);
 });
+
+//objects use key value parts
+let quoteList = [
+  { text: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+  { text: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+  { text: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+
+
+//WORKING ON HTTP REQUEST
+//LIKE GET REQUEST AND POST REQUEST
+//CREATE AN ENDPOINT TO GET THE QUOTES
+// the first argument to app.get is the name of the route
+// the secoond, is an anonymous function
+// arrow function syntax is () => {}, params go insiddee the ()
+app.get('/quotes', (req,res) => {
+  console.log('get request successful')
+  //use when you want to get info
+  //res.send sends a payload
+  res.send(quoteList)
+  // res.sendStatus(404)
+})
+
+// this is also an api, built an infrastructure
+app.post('/quotes', (req,res) => {
+  console.log('POST to /quotes success')
+  res.sendStatus(201)
+})
+
+
+
+
+
